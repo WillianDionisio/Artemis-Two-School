e7bdf7b3f240c8c2b9515ed3ddc1c13647e1bc3c
Willian Gonçalves Pereira Dionísio <pet0163507@aluno.firjansenaisesi.com.br>
2026-04-22 16:44:55 -0300

    swagger

--- demo/src/main/java/com/example/demo/configs/Swagger.java
+++ demo/src/main/java/com/example/demo/configs/Swagger.java
@@ -1,5 +1,10 @@
 package com.example.demo.configs;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+(info = @Info(title = "Artemis-two-shool", version = "1.0", description = "Sistema para artemis"))
 public class Swagger {
-    
+
 }
